e564aafea55310c389bfc56b6994f14a84c1968c
ZergeyS <zergeys@yandex.ru>
2022-06-09 01:47:37 +0300

    тест

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,17 @@
 public class Test {
+//
+//    public static void main(String[] args)
+//        {
+//        System.out.print("Hello, world!");
+//        }
+    public static void main(String[] args) {
+        int myInt = 10;
+        int myInt2 = 333;
+        int result = myInt2 / (myInt * myInt);
 
-    public static void main(String[] args)
-        {
-        System.out.print("Hello, world!");
-        }
+        System.out.println(result);
+//        System.out.println(myInt2);
+
+}
 
 }
